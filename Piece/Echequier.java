0c678a9ffe43cd5399bd9831e69d73fdc5e461b4
Celthans <celthans.dezan@etu.u-paris.fr>
2025-02-08 19:24:44 +0100

    CACA

--- Piece/Echequier.java
+++ Piece/Echequier.java
@@ -4,45 +4,52 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Echequier {
+  private final int MAX_CASE=8;
   private Map<String, Piece> plateau = new HashMap<>();
 
-  public Echequier() {
-    initialiserEchiquier();
+  public Echequier(){
+    initialiserEchequier();
   }
 
-  private void initialiserEchiquier() {
-    // Ajouter les pièces sur l’échiquier
-    for (int i = 0; i < 8; i++) {
-      plateau.put(getKey(i, 1), new Pion(Couleur.BLANC, i, 1));
-      plateau.put(getKey(i, 6), new Pion(Couleur.NOIR, i, 6));
+  private void initialiserEchequier(){
+
+    for (int i = 0; i< MAX_CASE; i++){
+      plateau.put(getKey(i, 1), new Pion(Couleur.BLANC,i,1));
+      plateau.put(getKey(i, 6), new Pion(Couleur.NOIR,i,6));
+
     }
-    plateau.put(getKey(4, 0), new Roi(Couleur.BLANC, 4, 0));
-    plateau.put(getKey(4, 7), new Roi(Couleur.NOIR, 4, 7));
-    plateau.put(getKey(3, 0), new Reine(Couleur.BLANC, 3, 0));
-    plateau.put(getKey(3, 7), new Reine(Couleur.NOIR, 3, 7));
+    plateau.put(getKey(4, 0), new Roi(Couleur.BLANC,4,0));
+    plateau.put(getKey(4, 7), new Roi(Couleur.NOIR,4,7));
+    plateau.put(getKey(3, 0), new Reine(Couleur.BLANC,3,0));
+    plateau.put(getKey(3, 7), new Reine(Couleur.NOIR,3,7));
   }
 
-  public boolean deplacerPiece(int x, int y, int newX, int newY) {
-    String key = getKey(x, y);
+  public boolean deplacerPiece(int x, int y, int newX, int newY){
+    String key = getKey(x,y);
     String newKey = getKey(newX, newY);
 
-    if (plateau.containsKey(key)) {
+    if(plateau.containsKey(key)){
       Piece piece = plateau.get(key);
-      if (piece.mouvementValide(newX, newY, this)) {
+      if (piece.mouvementValide(newX, newY, this)){
+
         plateau.remove(key);
         plateau.put(newKey, piece);
-        piece.deplacer(newX, newY);
+        piece.deplacer(newX,newY);
         return true;
+
       }
     }
     return false;
   }
 
-  public Piece getPiece(int x, int y) {
-    return plateau.get(getKey(x, y));
-  }
+  public Piece getPiece(int x, int y){
+    return plateau.get(getKey(x,y));
 
-  private String getKey(int x, int y) {
+  }
+  private String getKey(int x, int y){
     return x + "," + y;
   }
+
 }
+
+
--- Piece/EchequierUI.java
+++ Piece/EchequierUI.java
@@ -1,15 +1,26 @@
 package Piece;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.Node;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EchequierUI extends GridPane {
   private static final int TAILLE_CASE = 75;
+  private Echequier echequier;
+  private Map<Piece, ImageView> imagesPieces = new HashMap<>();
   private final int MAX_CASE = 8;
+  private Piece pieceSelectionne = null;
 
   public EchequierUI() {
+    this.echequier = new Echequier();
     dessinerEchequier();
+    placerPieces();
+    gererClics();;
   }
 
   private void dessinerEchequier(){
@@ -19,7 +30,7 @@ public class EchequierUI extends GridPane {
         if ((l+c)% 2 == 0){
           caseEchequier.setFill(Color.WHITE);
         }else{
-          caseEchequier.setFill(Color.BLACK);
+          caseEchequier.setFill(Color.GRAY);
         }
         this.add(caseEchequier,c,l);
 
@@ -27,4 +38,60 @@ public class EchequierUI extends GridPane {
 
     }
   }
+
+  private void placerPieces(){
+
+    for(int x = 0; x < MAX_CASE; x++){
+      for(int y=0; y<MAX_CASE; y++){
+        Piece piece = echequier.getPiece(x,y);
+
+        if (piece != null ){
+          ImageView pieceImage = new ImageView(getImagePiece(piece));
+          pieceImage.setFitWidth(TAILLE_CASE);
+          pieceImage.setFitHeight(TAILLE_CASE);
+          imagesPieces.put(piece, pieceImage);
+          this.add(pieceImage, x, y );
+        }
+      }
+    }
+  }
+
+  private Image getImagePiece(Piece piece){
+
+    String nomFichier = switch (piece.getClass().getSimpleName()){
+
+      case "Roi"-> "roi";
+        case "Reine"-> "reine";
+          case "Tour"-> "tour";
+            case "Fou"-> "fou";
+              case "Cavalier"-> "cavalier";
+                case "Pion"-> "pion";
+                default -> "";
+
+    };
+    nomFichier += piece.getCouleur() == Couleur.BLANC ? "_blanc.png": "_noir.png";
+    return new Image(getClass().getResourceAsStream("/image/"+ nomFichier));
+  }
+  private void mettreAJourPlateau(){
+    this.getChildren().removeIf(node-> node instanceof ImageView);
+    placerPieces();
+  }
+
+  private void gererClics() {
+    this.setOnMouseClicked(event -> {
+      int colonne = (int) (event.getX() / TAILLE_CASE);
+      int ligne = (int) (event.getY() / TAILLE_CASE);
+
+      if (pieceSelectionne == null) {
+        pieceSelectionne = echequier.getPiece(colonne, ligne);
+      } else {
+        if (echequier.deplacerPiece(pieceSelectionne.x, pieceSelectionne.y, colonne, ligne)) {
+          mettreAJourPlateau();
+        }
+        pieceSelectionne = null;
+      }
+    });
+  }
+
+
 }
